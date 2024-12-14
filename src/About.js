6cfb224b87f06342420e609146bc732a70d34b91
madhav0407 <madhav.sood0407@gmail.com>
2024-12-14 10:43:22 +0530

    adding report

--- src/About.js
+++ src/About.js
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <div>
         <h3>Team Members:</h3>
-        <p>Srinivas Madhav Sood IMT2021009 Shlok Agrawal IMT2021103</p>
+        <p>Madhav Sood IMT2021009 Shlok Agrawal IMT2021103</p>
     </div>
   )
 }
